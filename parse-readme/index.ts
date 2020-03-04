0797e2b9a455f237075dfe363dd66effe646e6e2
Robert Molina <rmolinamir@gmail.com>
2020-03-04 00:40:22 -0500

    parse-readme init to parse images in markdown files of this repo recursively

---
+++ parse-readme/index.ts
@@ -0,0 +1,123 @@
+// Libraries
+import {
+  resolve,
+  join,
+} from 'path';
+import {
+  createWriteStream,
+  readFileSync,
+  promises,
+} from 'fs';
+import marked from 'marked';
+import axios from 'axios';
+import { JSDOM } from 'jsdom';
+
+interface IImage {
+  alt: string
+  title: string
+  src: string
+}
+
+const { readdir } = promises;
+
+/**
+ * Recursive Files Async Iterator Generator. Allow us to iterate over data that
+ * comes asynchronously, in this case the data will be our directories.
+ * Returns an Async Iterator of the files in every directory that
+ * is not a node module or a git folder.
+ * @param {string} dir - Directory.
+ */
+async function* getFiles(dir: string): AsyncGenerator<string> {
+  // A representation of a directory entry, as
+  // returned by reading from an fs.Dir.
+  const dirents = await readdir(dir, { withFileTypes: true });
+  for (const dirent of dirents) { // Base condition.
+    // Resolving the files and directories.
+    const res = resolve(dir, dirent.name);
+    // Do not get files if they're node modules or git folders.
+    const shouldNotGetFiles = (
+      res.includes('node_modules') ||
+      res.includes('.git')
+    )
+    // If it's a directory, recursively get more files.
+    if (dirent.isDirectory() && !shouldNotGetFiles) {
+      yield* getFiles(res);
+    } else {
+      yield res; // Return the file.
+    }
+  }
+}
+
+/**
+ * Parses the Markdown file into a HTML file, then creates a DOM object,
+ * then uses querySelectorAll to find all of the images.
+ * The images are processed to get their metadata, then we return an image
+ * array.
+ * @param {string} file - Markdown File.
+ */
+function findImagesInMarkdown(file: string): Array<IImage> {
+  const readMe = readFileSync(file, 'utf-8');
+  const readmeHtml = marked(readMe); // .md to .html.
+  const dom = new JSDOM(readmeHtml);// .html to DOM.
+  const images: Array<IImage> = Array // Parsing images in the DOM.
+    .prototype
+    .slice
+    .call(dom.window.document.querySelectorAll('img'))
+    .reduce((rawImages: Array<IImage>, image: any) => {
+      rawImages.push({
+        alt: image.getAttribute('alt') as string,
+        title: image.getAttribute('title') as string,
+        src: image.getAttribute('src') as string,
+      });
+      return rawImages;
+    }, []);
+  return images;
+}
+
+/**
+ * Downloads all of the found images in the markdown files,
+ * then save the data in the passed directory.
+ * @param {IImage[]} images - Array of images attrs.
+ * @param {string} saveDirectory - Download directory.
+ */
+async function downloadImages(images: IImage[], saveDirectory: string) {
+  for (let i = 0; i < images.length; i++) {
+    const { src, title, alt } = images[i];
+    const filename = `${title}_${alt}.png`
+      .replace(/[/\\?%*:|"<>]/g, '')
+      .replace(' ', '-');
+    const result = await axios({ url: src, responseType: 'stream' });
+    // Downloading the images, then saving them.
+    await new Promise((resolve, reject) => {
+      result.data
+        .pipe(createWriteStream(
+          join(saveDirectory, filename),
+          {
+            flags: 'w+',
+          },
+        ))
+        .on('finish', resolve)
+        .on('error', (e: Error) => reject(e.message));
+    });
+  }
+}
+
+/**
+ * Recursively downloads all images then saves
+ */
+async function recursiveMarkdownImageSearch() {
+  for await (const file of getFiles('..')) {
+    const parsedFile = file.split('.');
+    const fileFormat = parsedFile[parsedFile.length - 1];
+    if (fileFormat === 'md') {
+      const images = findImagesInMarkdown(file);
+      const directory = file.split('\\');
+      const saveDirectory = directory
+        .slice(0, directory.length - 1)
+        .join('\\');
+      await downloadImages(images, saveDirectory);
+    }
+  }
+}
+
+recursiveMarkdownImageSearch();
